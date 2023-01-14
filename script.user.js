775a083c04eb75fc3615e1079182c00cafd78c14
nailuoGG <nailuogg@gmail.com>
2023-01-14 23:10:59 +0800

    fix: filename

--- script.user.js
+++ script.user.js
@@ -2,7 +2,7 @@
 // @name         记录当前的Tab
 // @namespace    http://tampermonkey.net/
 // @version      0.1.2
-// @downloadURL https://github.com/nailuoGG/deno-bridge-echo/raw/main/monkey.js
+// @downloadURL https://github.com/nailuoGG/deno-bridge-echo/raw/main/script.user.js
 // @description Store Latest visited Tab into local HTTP server
 // @author       liujiale
 // @connect      localhost
