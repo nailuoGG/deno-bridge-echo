a830823075d6a2362b30fb65e03e380af7bd857c
nailuoGG <nailuogg@gmail.com>
2023-01-14 23:10:18 +0800

    fix: delete invalid meta tag

--- script.user.js
+++ script.user.js
@@ -2,7 +2,6 @@
 // @name         记录当前的Tab
 // @namespace    http://tampermonkey.net/
 // @version      0.1.2
-// @updateURL https://github.com/nailuoGG/deno-bridge-echo/raw/main/monkey.js
 // @downloadURL https://github.com/nailuoGG/deno-bridge-echo/raw/main/monkey.js
 // @description Store Latest visited Tab into local HTTP server
 // @author       liujiale
