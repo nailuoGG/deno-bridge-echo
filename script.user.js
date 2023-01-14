c23d210ca5e20ab649831d1b1f0fcc099c748ab5
nailuoGG <nailuogg@gmail.com>
2023-01-14 23:12:05 +0800

    fix: update version

--- script.user.js
+++ script.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         记录当前的Tab
 // @namespace    http://tampermonkey.net/
-// @version      0.1.2
+// @version      0.1.3
 // @downloadURL https://github.com/nailuoGG/deno-bridge-echo/raw/main/script.user.js
 // @description Store Latest visited Tab into local HTTP server
 // @author       liujiale
